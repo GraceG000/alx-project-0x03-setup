3c769a42fa63355ebdce1f8cee980b951cdf985d
Grace Ghansah <gracegh@stlghana.com>
2025-11-23 09:18:47 +0000

    update

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from "../interface/index.tsx -";
+import { ButtonProps } from "../interface/index.tsx";
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
